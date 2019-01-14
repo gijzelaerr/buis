f294cdf2d8b6da0fa77df2487470fe45aec1da53
Gijs Molenaar <gijs@pythonic.nl>
2019-01-14 14:28:05 +0200

    fix test

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from .models import Repository
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+USER = 'test'
+PASS = 'test'
 
 
 def create_repository(url):
@@ -8,23 +12,29 @@ def create_repository(url):
 
 
 class RepositoryIndexViewTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user(USER, 'temporary@gmail.com', PASS)
+
     def test_no_repositories(self):
         """
         If no repo exist, an appropriate message is displayed.
         """
-        response = self.client.get(reverse('scheduler:index'))
+        self.client.login(username=USER, password=PASS)
+        response = self.client.get(reverse('scheduler:repo_list'), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No repositories are available.")
-        self.assertQuerysetEqual(response.context['object_list'], [])
+        self.assertQuerysetEqual(response.context_data['object_list'], [])
 
     def test_multiple_repositories(self):
         """
         The index page should display multiple questions.
         """
+        self.client.login(username=USER, password=PASS)
         create_repository('git@bla')
         create_repository('git@bla')
-        response = self.client.get(reverse('scheduler:index'))
+        response = self.client.get(reverse('scheduler:repo_list'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(list(response.context['object_list'])), 2)
+        self.assertEqual(len(list(response.context_data['object_list'])), 2)
 
 
