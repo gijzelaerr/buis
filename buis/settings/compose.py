2de1f7793584f05e77365aa9b386a0df14c58aa7
Gijs Molenaar <gijs@pythonic.nl>
2019-03-15 17:16:41 +0100

    add allowed hosts

--- buis/settings/compose.py
+++ buis/settings/compose.py
@@ -19,3 +19,12 @@ DATABASES = {
 
 TOIL_BIN = '/usr/local/bin/toil-cwl-runner'
 CWLTOOL_BIN = '/usr/local/bin/cwltool'
+
+
+ALLOWED_HOSTS = [
+    'happili-01:8000',
+    'happili-01',
+    'localhost',
+    '127.0.0.1',
+    'demo.pythonic.nl',
+]
